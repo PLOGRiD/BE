e6701681d374cd4cdfd08467e98dfaa94ba21d88
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-05-06 12:50:31 +0900

    Initial commit

---
+++ src/main/java/com/example/plogrid/PlogridApplication.java
@@ -0,0 +1,13 @@
+package com.example.plogrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlogridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlogridApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plogrid/PlogridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plogrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlogridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
