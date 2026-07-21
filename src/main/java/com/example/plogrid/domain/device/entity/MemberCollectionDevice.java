52df157114b346a31bd65e3ae550e108e6bb7808
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-07-21 23:05:59 +0900

    feat: 디바이스-사용자 매핑 엔티티(MemberCollectionDevice) 사용자ID, 디바이스ID 복합 유니크 제약 추가

--- src/main/java/com/example/plogrid/domain/device/entity/MemberCollectionDevice.java
+++ src/main/java/com/example/plogrid/domain/device/entity/MemberCollectionDevice.java
@@ -10,11 +10,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"member_id", "collection_device_id"}))
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberCollectionDevice extends BaseEntity {
