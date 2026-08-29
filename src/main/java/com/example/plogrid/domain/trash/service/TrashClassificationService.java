c3bd2532cb9beb0ac68a6dedd4501e13bb5d2812
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-08-29 22:17:17 +0900

    feat: 쓰레기 분석 처리를 비동기로 전환

--- src/main/java/com/example/plogrid/domain/trash/service/TrashClassificationService.java
+++ src/main/java/com/example/plogrid/domain/trash/service/TrashClassificationService.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,6 +33,7 @@ public class TrashClassificationService {
 	private final S3Uploader s3Uploader;
 	private final ApplicationEventPublisher eventPublisher;
 
+	@Async("taskExecutor")
 	public void trashClassification(
 		TrashRequestDTO.WasteClassification request, Plogging plogging) throws IOException {
 
